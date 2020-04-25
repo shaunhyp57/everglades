ad275df585eaec242f93a5f59166b6ebf03ffe8f
Shauna Hyppolite <35539868+shaunhyp57@users.noreply.github.com>
2020-04-25 01:49:31 -0400

    Update and rename test_battle_WriteDsets_Brian.py to test_battle_WriteDsets.py

--- real_time_engine/game-logic-master/test_battle_WriteDsets.py
+++ real_time_engine/game-logic-master/test_battle_WriteDsets.py
@@ -15,20 +15,6 @@ from RT_Engine_Class import *
 # Agent files must include a class of the same name with a 'get_action' function
 # Do not include './' in file path
 agent0_file = ('agents/random_actions.py')
-#agent0_file = ('agents/all_cycle.py')
-#agent0_file = ('agents/base_rushV1.py')
-#agent0_file = ('agents/Cycle_BRush_Turn50.py')
-#agent0_file = ('agents/Cycle_BRush_Turn25.py')
-#agent0_file = ('agents/cycle_targetedNode11.py')
-
-
-#agent1_file = 'agents/same_commands.py'
-#agent1_file = 'agents/random_actions.py'
-#agent1_file = ('agents/all_cycle.py')
-#agent1_file = ('agents/base_rushV1.py')
-#agent1_file = ('agents/Cycle_BRush_Turn50.py')
-#agent1_file = ('agents/cycle_targetedNode11.py')
-#agent1_file = ('agents/Cycle_BRush_Turn25.py')
 agent1_file = ('agents/cycle_targetedNode11P2.py')
 
 config_dir = 'config/'
@@ -41,7 +27,7 @@ debug = 1
 #C:\Users\brian\OneDrive\Documents\GitHub\LMCO-Everglades-Robot-Behavior-Analytics\real_time_engine\game-logic-master
 ## Specific Imports
 agent0_name, agent0_extension = os.path.splitext(agent0_file)
-agent0_mod = importlib.import_module(agent0_name.replace('/','.'), package='.Users.brian.OneDrive.Documents.GitHub.LMCO-Everglades-Robot-Behavior-Analytics.real_time_engine.game-logic-master')
+agent0_mod = importlib.import_module(agent0_name.replace('/','.'), package = '.Users.brian.OneDrive.Documents.GitHub.LMCO-Everglades-Robot-Behavior-Analytics.real_time_engine.game-logic-master')
 #agent0_mod = importlib.import_module(agent0_name.replace('/','.'), package='.Users.brian.game-logic-master.agents.random_actions')
 agent0_class = getattr(agent0_mod, os.path.basename(agent0_name))
 
@@ -71,47 +57,38 @@ observations = env.reset(
 )
 
 actions = {}
-#Engine = RT_Engine()
-## Game Loop
+
+## game loop
 done = 0
-turn_number=1
+turn_number = 1
 
 while not done:
     if debug:
-        #env.game.debug_state()
         #data pipeline from server.py
-        data_List=env.game.get_output()
+        data_List = env.game.get_output()
         
-        
-
     for pid in players:
-        actions[pid] = players[pid].get_action( observations[pid] )
+        actions[pid] = players[pid].get_action(observations[pid])
 
     observations, reward, done, info = env.step(actions)
     
     print("***************************")
     
     if turn_number == 1:
-        Running_Game=GameState(data_List)
+        Running_Game = GameState(data_List)
     #update games state after a turn runs    
-    Running_Game.update_game_state(data_List,turn_number)
-    featureList=Running_Game.features(turn_number)
+    Running_Game.update_game_state(data_List, turn_number)
+    featureList = Running_Game.features(turn_number)
     
     ### Show how refined list works#####
     #print(refined_list(data_List['NODE_Knowledge'],turn_number)) #all data for a turn
     #print(refined_list(data_List['NODE_Knowledge'],turn_number)[0]) # first line of data for turn
     #print(refined_list(data_List['NODE_Knowledge'],turn_number)[0][1]) #first line cell 1 (2 in csv)
     #print(refined_list(data_List['NODE_Knowledge'],turn_number)[0][1][0]) #all cells are a list/tuple so even if it is one element it must be ref. with zero
-   
-  
-    print("Turn >>>>",turn_number)
-    #predictions = Engine.prediction_alg(math.ceil(float(featureList[0])/2)-1,[featureList[1:]])
-    #print(predictions)
-    
+    print("Turn >>>>", turn_number)
     print("***************************")
     
-    turn_number+=1
-###Used to write turn by turn data to the CSVs which are used to build models
+    turn_number += 1
+
+### used to write turn by turn data to the CSVs which are used to build models
 Running_Game.print_features_CSV()
-
-
