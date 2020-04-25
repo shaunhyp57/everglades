677938d21fcd321ca76ee4fb9ea6e7acba723be7
Shauna Hyppolite <35539868+shaunhyp57@users.noreply.github.com>
2020-04-25 01:30:24 -0400

    Update model_training.py

--- real_time_engine/game-logic-master/model_training.py
+++ real_time_engine/game-logic-master/model_training.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import scale
 rootdir = 'datasets'
 
 def list_files(rootdir):
-    list=[]
+    list = []
     for root, dirs, files in os.walk(rootdir):
         list = files
     print(list)
@@ -20,9 +20,10 @@ def list_files(rootdir):
    
 def train_save_model(files):
     i = .01
+    
     for file in files:
         print(file)
-        train(file,i)
+        train(file, i)
         i = i + .01
 
 def train(file_name, i):
@@ -32,10 +33,10 @@ def train(file_name, i):
     cols = ['Winner','Turn_Number','Point_Diff','Player1_Base_Control','Player2_Base_Control','Unit_Diff','Unit_Health_Diff']
     
     #store csv as a data frame
-    df = pd.read_csv(file, names = cols, header = None,usecols = cols)
+    df = pd.read_csv(file, names = cols, header = None, usecols = cols)
     
     #Set features
-    feature_cols = ['Point_Diff','Player1_Base_Control','Player2_Base_Control','Unit_Diff','Unit_Health_Diff']
+    feature_cols = ['Point_Diff', 'Player1_Base_Control', 'Player2_Base_Control', 'Unit_Diff', 'Unit_Health_Diff']
 
     #declare X matrix and y vector 
     X = df[feature_cols]
@@ -53,7 +54,7 @@ def train(file_name, i):
     model_sigmoid = CalibratedClassifierCV(model, cv = 2, method = 'isotonic')
     model_sigmoid.fit(X, y)
     
-    filename='models/model_'+str(round(i,2))+'.PICKLE'
+    filename = 'models/model_' + str(round(i,2)) + '.PICKLE'
     
     pickle.dump(model_sigmoid, open(filename,'wb'))
 
