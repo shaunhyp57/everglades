4bfc8f3f1f5a66e4202f223ba545df24ef487646
Shauna Hyppolite <35539868+shaunhyp57@users.noreply.github.com>
2020-04-25 00:55:15 -0400

    fixed f1-score calculation

--- real_time_engine/game-logic-master/Engine_Analysis/RTE_Analysis.py
+++ real_time_engine/game-logic-master/Engine_Analysis/RTE_Analysis.py
@@ -3,7 +3,7 @@ import csv
 rootdir = 'game_prediction_info'
 
 def list_files(rootdir):
-    list=[]
+    list = []
     r = []
     i=0
     
@@ -14,21 +14,20 @@ def list_files(rootdir):
 
     for root, dirs, files in os.walk(rootdir):
         for name in files:
-            fullPath=(os.path.join(root, name))
+            fullPath = (os.path.join(root, name))
             print(fullPath)
             
-            fullPathList=[]
-            fullPathList=fullPath.split('/')
+            fullPathList = []
+            fullPathList = fullPath.split('/')
             
-            #print(fullPathList[1])
-            list=confusion_matrix_data(fullPath,list)
+            list = confusion_matrix_data(fullPath, list)
     
     filestr = ("predictionInfoSummary.csv")        
     fileCSV = open(filestr,"w+")
     
     #write headers
     fileCSV.write('TurnNumber'+','+'TruePositive'+','+'FalsePositive'+','+'TrueNegative'+','+'FalseNegative'+','+
-    'Accuracy'+','+'Specificity'+','+'Sensitivity'+','+'Precision'+','+'F2Score')
+    'Accuracy'+','+'Specificity'+','+'Sensitivity'+','+'Precision'+','+'F1Score')
     fileCSV.write("\n")
     
     i = 1
@@ -53,7 +52,7 @@ def list_files(rootdir):
         else:
             precision = (int(item[0]))/(int(item[0]) + int(item[1]))
       
-       #f2 score
+       #f1 score
        #division by error issue, needs to be looked into more. 
         if (float(precision) + float(sensitivity)) == 0.0 or precision * sensitivity == 0:
             f1_score = 0
@@ -70,8 +69,9 @@ def list_files(rootdir):
         fileCSV.write(str(f1_score))
         fileCSV.write("\n")
         i += 1
-def confusion_matrix_data(path,list):
-    File = open(path,"r")
+        
+def confusion_matrix_data(path, list):
+    File = open(path, "r")
     turn_info_list=[]
     
     File.readline()
@@ -80,20 +80,21 @@ def confusion_matrix_data(path,list):
     for row in File:
         tempRow = {}
         tempRow = row.split(",")
+        
         if int(tempRow[1]) == 1 and int(tempRow[4]) == 1:
-            
             list[int(tempRow[0]) - 1][0] += 1
-            print("TP>>>>>>>>>>>>>>>>>>>>>>>>>>",list[int(tempRow[0]) - 1])
+            print("TP>>>>>>>>>>>>>>>>>>>>>>>>>>", list[int(tempRow[0]) - 1])
         elif int(tempRow[1]) == 1 and  int(tempRow[4]) == 0:
-            list[int(tempRow[0])-1][1] += 1
+            list[int(tempRow[0]) - 1][1] += 1
             print("FP")
         elif int(tempRow[1]) == 0 and  int(tempRow[4]) == 0:
-            list[int(tempRow[0])-1][2] += 1
+            list[int(tempRow[0]) - 1][2] += 1
         elif int(tempRow[1]) == 0 and  int(tempRow[4]) == 1:
-            list[int(tempRow[0])-1][3] += 1
+            list[int(tempRow[0]) - 1][3] += 1
             print("FN")
         else:
             print("Error")
+            
     File.close()
     
     return list
